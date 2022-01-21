3e6e7598cb52534676c7192e6fa986c1f068c03e
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-01-21 02:05:30 +0100

    Add csvtojson Add basic idea for converter function

--- src/App.js
+++ src/App.js
@@ -1,51 +1,24 @@
-import {TimeLineChart} from "./components/TimeLineChart";
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import { TimeLineChart } from "./components/TimeLineChart";
+import * as csv from "csvtojson"
+import { useEffect, useState } from "react";
+import * as file from "./file.csv"
 function App() {
-   
-  return <TimeLineChart data={data} />
+    const [data, setData] = useState()
+    const reader = new FileReader()
+    useEffect(() => {
+        async function loadCSV() {
+            const fetchData = await csv().fromString(file)
+            console.log(fetchData)
+            setData(fetchData)
+        }
+        loadCSV()
+    }, [])
+
+    return <>
+
+        <p>{JSON.stringify(data)}</p>
+        <TimeLineChart data = { data } /> 
+    </>
 }
 
 export default App;
