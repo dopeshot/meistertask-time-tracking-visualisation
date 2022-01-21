e80c9f93d829cc4661f2554d058ad0fedb83d45a
Lars Flieger <a.purenetwork@gmail.com>
2022-01-21 02:09:54 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import { TimeLineChart } from "./components/TimeLineChart";
-import * as csv from "csvtojson"
-import { useEffect, useState } from "react";
-import * as file from "./file.csv"
+import { useState } from "react";
+import * as XLSX from 'xlsx';
 function App() {
-    const [data, setData] = useState()
-    const reader = new FileReader()
-    useEffect(() => {
-        async function loadCSV() {
-            const fetchData = await csv().fromString(file)
-            console.log(fetchData)
-            setData(fetchData)
+  const [columns, setColumns] = useState([]);
+  const [data, setData] = useState([]);
+ 
+  // process CSV data
+  const processData = dataString => {
+    const dataStringLines = dataString.split(/\r\n|\n/);
+    const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+ 
+    const list = [];
+    for (let i = 1; i < dataStringLines.length; i++) {
+      const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+      if (headers && row.length == headers.length) {
+        const obj = {};
+        for (let j = 0; j < headers.length; j++) {
+          let d = row[j];
+          if (d.length > 0) {
+            if (d[0] == '"')
+              d = d.substring(1, d.length - 1);
+            if (d[d.length - 1] == '"')
+              d = d.substring(d.length - 2, 1);
+          }
+          if (headers[j]) {
+            obj[headers[j]] = d;
+          }
         }
-        loadCSV()
-    }, [])
+ 
+        // remove the blank rows
+        if (Object.values(obj).filter(x => x).length > 0) {
+          list.push(obj);
+        }
+      }
+    }
+ 
+    // prepare columns list from headers
+    const columns = headers.map(c => ({
+      name: c,
+      selector: c,
+    }));
+ 
+    setData(list);
+    setColumns(columns);
+  }
+ 
+  // handle file upload
+  const handleFileUpload = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      /* Parse data */
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: 'binary' });
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      /* Convert array of arrays */
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+      processData(data);
+    };
+    reader.readAsBinaryString(file);
+  }
 
-    return <>
-
-        <p>{JSON.stringify(data)}</p>
-        <TimeLineChart data = { data } /> 
-    </>
+  return <>
+    <input
+      type="file"
+      accept=".csv,.xlsx,.xls"
+      onChange={handleFileUpload}
+    />
+    <p>{JSON.stringify(data)}</p>
+    <TimeLineChart data={data} />
+  </>
 }
 
 export default App;
